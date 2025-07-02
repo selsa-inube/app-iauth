81cf987f72ad2d64b70343367a912dd616fefba0
Miller Ostos <millerandresoc@gmail.com>
2025-07-02 06:58:33 -0500

    refactor: delete colors tokens

--- src/config/login/mask/index.ts
+++ src/config/login/mask/index.ts
@@ -1,13 +1,9 @@
-import { inube } from "@inubekit/inubekit"
-
 const mask = {
     maskBackground: {
-        backgroundColor: inube.palette.neutralAlpha.N30A ,
         zIndex: -1
     },
     maskModalWarning: {
-        backgroundColor: inube.palette.neutralAlpha.N100A,
-        zIndex: 99
+        zIndex: 1
     }
 
 }
