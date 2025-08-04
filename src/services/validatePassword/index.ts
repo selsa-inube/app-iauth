7c3f04ada840f4c7f5947a5bc95ff725ba45de09
RubenSantiagoUrrego <rurrego@sistemasenlinea.com.co>
2025-08-04 13:38:31 -0500

    fix: 🐛 improve type assertion and formatting in axios instance
    \

--- src/services/validatePassword/index.ts
+++ src/services/validatePassword/index.ts
@@ -3,19 +3,22 @@ import { IValidate } from "@ptypes/api/IValidations";
 import { IValidateParams } from "@ptypes/api/IValidationsParams";
 import { callbackUrl } from "@config/environment";
 
-const validatePassword = async ( params:IValidateParams ): Promise<IValidate> => {
-    let {
-        password,
-        username
-    } = params;
-    
-    if (password && username) {
-        password = btoa(password);
-        username = btoa(username);
-    }
-    
-    const data = await axiosInstance.post<IValidate>('/user-accounts/', { userAccount: username, authenticationCredential: password, callbackUrl: callbackUrl });
-    return data;
+const validatePassword = async (
+  params: IValidateParams,
+): Promise<IValidate> => {
+  let { password, username } = params;
+
+  if (password && username) {
+    password = btoa(password);
+    username = btoa(username);
+  }
+
+  const { data } = await axiosInstance.post<IValidate>("/user-accounts/", {
+    userAccount: username,
+    authenticationCredential: password,
+    callbackUrl: callbackUrl as string,
+  });
+  return data;
 };
 
 export { validatePassword };
